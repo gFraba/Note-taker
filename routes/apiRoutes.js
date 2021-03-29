cfc9954dd6ac6c622ef7d6e6aae08e028e940bbf
gfraba <joshua.gagle@gmail.com>
2021-03-29 17:35:00 -0400

    get and delete tested and working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,18 @@
 const fs = require('fs');
 
 module.exports = function (app) {
+    
+    
+    app.get("/api/notes", function (req, res) {
+        // console.log("Getting Notes");
+        fs.readFile("db/db.json", "utf8", (err, data) => {
+            if (err) throw err;
+            let notes = JSON.parse(data);
+            res.json(notes);
+        });
+    });
 
+    
     app.post("/api/notes", function (req, res) {
         fs.readFile("db/db.json", "utf8", (err, data) => {
             if (err) throw err;
@@ -16,8 +27,36 @@ module.exports = function (app) {
             notes.push(newNote);
 
             fs.writeFileSync("db/db.json", JSON.stringify(notes), "utf8", (err, data) => {
+                console.log("New note added");
                 if (err) throw err;
-                console.log("New note successfully added!");
+                
+            });
+
+            res.json(notes);
+        });
+    });
+
+
+    app.delete("/api/notes/:id", function (req, res) {
+        fs.readFile("db/db.json", "utf8", (err, data) => {
+            if (err) throw err;
+
+            let notes = JSON.parse(data);
+            let notesId = req.params.id;
+            let newNotesId = 0;
+
+            notes = notes.filter(currNote => {
+                return currNote.id != notesId;
+            });
+
+            for (currNote of notes) {
+                currNote.id = newNotesId.toString();
+                newNotesId++;
+            }
+
+            fs.writeFileSync("db/db.json", JSON.stringify(notes), "utf8", (err, data) => {
+                if (err) throw err;
+                console.log("Note deleted");
             });
 
             res.json(notes);
